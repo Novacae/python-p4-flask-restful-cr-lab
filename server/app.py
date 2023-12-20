39bec5679be5a7ad72d8405e743bf9482ee9abd2
aman githinji <amangithinji09@gmail.com>
2023-12-20 18:19:23 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -17,11 +16,47 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+
+    def get(self):
+
+        plants = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(
+            jsonify(plants),
+            200
+        )
+        return response
+    
+    def post(self):
+        data = request.get_json()
+        new_plant = Plant(
+            name = data['name'],
+            image = data['image'],
+            price = data['price']
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(new_plant.to_dict()),
+            201
+        )
+        return response
+
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+
+    def get(self,id):
+
+        plant = Plant.query.filter_by(id=id).first()
+        response = make_response(
+            jsonify(plant.to_dict()),
+            200
+        )
+        return response
         
+api.add_resource(PlantByID , '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
